393a233d50f7eeb8d118161e7563fa231104de6b
mairillita <134753300+mairillita@users.noreply.github.com>
2023-08-20 20:50:55 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Venus Betaza</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<header>
+    <a href="index.php">
+      <div class="titulo">
+        <img src="img/logo.png" class="logo">
+        <h1>Venus Betaza</h1>
+      </div>
+    </a>
+
+    <div class="barra">
+      <nav>
+        <ul>
+          <li class="subContenidos"><a href="galeria.html">Galería de trabajos</a></li>
+          <li class="subContenidos"><a href="#contact">Contacto</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+    <section class="bienvenida">
+    <h1>¡Bienvenidos a Removedora de Escombros "Venus Betaza"!</h1>
+    </section>
+    <section class="services">
+        <h2>Nuestros Servicios</h2>
+        <ul>
+            <li><b>Remoción de Escombros a Medida:</b></li>
+              <p>Sea cual sea el tamaño de su espacio, 
+                desde renovaciones en el hogar hasta construcciones 
+                escolares, tenemos la experiencia y las cuadrillas adecuadas
+                para eliminar escombros de manera rápida y 
+                segura. Nos adaptamos a sus necesidades específicas, 
+                brindando soluciones personalizadas.</p>
+
+            <li><b>Profesionalismo Garantizado</b></li>
+            <p>Nuestras cuadrillas y mano de obra están
+              comprometido con la excelencia en cada paso del proceso. 
+              Desde la planificación inicial hasta la limpieza final, 
+              puede confiar en nuestra dedicación para entregar resultados 
+              impecables y satisfactorios.</p>
+        </ul>
+    </section>
+
+    <section class="elegirnos">
+    <h2>¿Por qué elegirnos?</h2>
+    <p>Sabemos que tiene opciones, pero con <b>Removedora "Venus Betaza"</b>, no 
+    solo obtiene un servicio de calidad, sino una experiencia integral que le brinda 
+    tranquilidad y resultados satisfactorios. Nos preocupamos por cada detalle y estamos 
+    comprometidos con su satisfacción total.</p><br>
+    <p>Explore nuestro sitio web para obtener más información sobre 
+      nuestros servicios, ver ejemplos de proyectos anteriores y 
+      ponerse en contacto con nosotros para obtener una cotización 
+      personalizada. No importa cuán grande o pequeño sea su proyecto 
+      de remoción de escombros, estamos aquí para convertir su visión 
+      en realidad.</p>
+    </section>
+
+    <section class="final">
+      <p>Atentamente,<br>
+      El Equipo de <b>Removedora "Venus Betaza"</b><br>
+      Tu Socio en Remoción de Escombros
+      </p>
+      <h4>¡Gracias por visitarnos y esperamos tener el privilegio de servirle pronto!</h4>
+    </section>
+
+    <section class="contact" id="contact">
+        <h2>Contacto</h2>
+        <p>¡Estamos listos para ayudarte en tu próximo proyecto! Contáctanos:</p>
+        <p>Teléfono: (921) 100-7296</p>
+        <p>Email: omvdeveracruz@hotmail.com</p>
+        <p>somos de coatzacoalcos, Veracruz</p>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Removedora "Venus Betaza". Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
